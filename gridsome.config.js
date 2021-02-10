d4862b1c9060815f194be1e5b2c35849e5040b0b
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-02-11 02:00:37 +0800

    add markdown templates

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,8 +1,3 @@
-// This is where project configuration and plugin options are located.
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
 const tailwind = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss");
 
@@ -13,7 +8,38 @@ if (process.env.NODE_ENV === "production")
 
 module.exports = {
   siteName: "Gridsome",
+  siteDescription:
+    "Bleda is a blog starter kit for Gridsome, the Vue.js static site generator. It's inspired by Attila for Ghost, and styled with Tailwind CSS.",
+  siteUrl: "https://gridsome-starter-bleda.netlify.com",
+  titleTemplate: `%s | Bleda`,
+  icon: "src/favicon.png",
+
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: ["gridsome-plugin-remark-prismjs-all"],
+    },
+  },
   plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "./content/posts/**/*.md",
+        typeName: "Post",
+        refs: {
+          tags: {
+            typeName: "Tag",
+            create: true,
+          },
+          author: {
+            typeName: "Author",
+            create: true,
+          },
+        },
+      },
+    },
     {
       use: "@gridsome/plugin-critical",
       options: {
@@ -23,6 +49,11 @@ module.exports = {
       },
     },
   ],
+  templates: {
+    Post: "/:title",
+    Tag: "/tag/:id",
+    Author: "/author/:id",
+  },
   css: {
     loaderOptions: {
       postcss: {
