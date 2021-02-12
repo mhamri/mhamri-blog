e212b736b94d39a0a6dac405a98a7e0029b0d0c3
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-02-12 23:56:56 +0800

    add inter font

--- src/main.js
+++ src/main.js
@@ -8,4 +8,13 @@ import DefaultLayout from "~/layouts/Default.vue";
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
+
+  head.htmlAttrs = { lang: "en", class: "h-full" };
+  head.bodyAttrs = { class: "antialiased font-serif" };
+
+  head.link.push({
+    rel: "stylesheet",
+    href:
+      "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap",
+  });
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+
+      // serif: ["Inter", "sans-serif"],
+
+      // mono: ["Inter", "sans-serif"],
+
+      // display: ["Inter", "sans-serif"],
+
+      // body: ["Inter", "sans-serif"],
+    },
+
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
