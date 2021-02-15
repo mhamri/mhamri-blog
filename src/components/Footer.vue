dae91675321333681f57f69ede7f153b47e54962
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-02-15 23:36:02 +0800

    clean up footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,12 @@
 <template>
   <footer class="text-gray-700 text-sm leading-normal flex flex-wrap justify-between mx-auto max-w-3xl px-6 sm:px-12">
     <div class="w-full sm:w-1/2 mb-4 sm:mb-0">
-      <p><a href="https://github.com/cossssmin/gridsome-starter-bleda" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">Bleda</a> by <a href="https://twitter.com/cossssmin" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">@cossssmin</a></p>
-      <p>Built with <a href="https://gridsome.org" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">Tailwind CSS</a>.</p>
+      <a href="https://www.linkedin.com/in/mhamri/">
+        <img
+          src="https://img.shields.io/badge/-mhamri-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/mhamri/"
+          alt="Mohammad Hossein Amri linkedin badge"
+        />
+      </a>
     </div>
     <div class="w-full sm:w-1/2">
       <nav>
@@ -10,9 +14,9 @@
           <li class="px-2">
             <g-link to="/" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">Home</g-link>
           </li>
-          <li class="px-2">
+          <!-- <li class="px-2">
             <g-link to="/about/" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">About</g-link>
-          </li>
+          </li> -->
           <li class="px-2">
             <a href="/sitemap.xml" class="border-b border-transparent hover:border-gray-400 transition-colors duration-300">Sitemap</a>
           </li>
@@ -26,8 +30,7 @@
 </template>
 
 <script>
-
 export default {
-  name: 'Footer',
-}
+  name: 'Footer'
+};
 </script>
